499635f04808aa3d35555887ac73a7748d54a483
arpithadfs <arpitha@dfrozen.com>
2020-06-05 12:49:30 +0530

    initial commit

---
+++ contact.php
@@ -0,0 +1,83 @@
+<?php
+if($_POST)
+{
+    $to_Email   	= "dhanraj@dfrozensoft.com"; //Replace with recipient email address
+    $subject        = 'D FROZEN SOFT PVT. LTD. - New Contact Inquiry'; //Subject line for emails
+    
+
+    //check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+
+        //exit script outputting json data
+        $output = json_encode(
+        array(
+            'type'=>'error',
+            'text' => 'Request must come from Ajax'
+        ));
+
+        die($output);
+    }
+
+    //check $_POST vars are set, exit if any missing
+    if(!isset($_POST["first_name"]) || !isset($_POST["last_name"]) || !isset($_POST["email"]) || !isset($_POST["phone"]) || !isset($_POST["message"]))
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Input fields are empty!'));
+        die($output);
+    }
+
+    //Sanitize input data using PHP filter_var().
+    $user_Fname        = $_POST["first_name"];
+    $user_Lname       = $_POST["last_name"];
+    $user_Email =  $_POST["email"];
+    $user_Phone =  $_POST["phone"];
+    $user_Message     = $_POST["message"];
+
+    //additional php validation
+    if(strlen($user_Fname)<3) // If length is less than 3 it will throw an HTTP error.
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
+        die($output);
+    }
+    if(!filter_var($user_Email, FILTER_VALIDATE_EMAIL)) //email validation
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Please enter a valid email!'));
+        die($output);
+    }
+
+    if(strlen($user_Message)<5) //check emtpy message
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
+        die($output);
+    }
+
+
+    $message_Body = "<strong>First Name: </strong>". $user_Fname ."<br>";
+    $message_Body .= "<strong>Last Name: </strong>". $user_Lname ."<br>";
+    $message_Body .= "<strong>Email: </strong>". $user_Email ."<br>";
+    $message_Body .= "<strong>Phone: </strong>". $user_Phone ."<br>";
+    $message_Body .= "<strong>Message: </strong>". $user_Message ."<br>";
+
+
+
+    $headers = "From: " . strip_tags($user_Email) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($user_Email) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    //proceed with PHP email.
+    $headers = 'From: '.$user_Email.'' . "\r\n" .
+    'Reply-To: '.$user_Email.'' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    $sentMail = @mail($to_Email, $subject, $message_Body, $headers);
+
+    if(!$sentMail)
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
+        die($output);
+    }else{
+        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$user_Name .' Thank you for contacting us.'));
+        die($output);
+    }
+}
+?>
